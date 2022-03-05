7423484e0e5b3456a0d6109972607ceaf4aa2190
Wonwoo Choi <chwo9843@gmail.com>
2022-03-05 17:38:26 +0900

    Initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+variable "aws_access_key_id" {
+  description = "Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "Secret Access Key"
+  type        = string
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region     = "ap-northeast-2"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
