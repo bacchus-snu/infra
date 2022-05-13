625986a49f8fa45e696f43421e940e29da16e864
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2022-05-13 20:38:11 +0900

    Recreate EKS cluster

---
+++ eks.tf
@@ -0,0 +1,74 @@
+module "eks_bacchus_dev" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.20"
+
+  cluster_name                    = "bacchus-dev"
+  cluster_version                 = "1.22"
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    vpc-cni = {
+      addon_version     = "v1.11.0-eksbuild.1"
+      resolve_conflicts = "OVERWRITE"
+    }
+    coredns = {
+      addon_version     = "v1.8.7-eksbuild.1"
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {
+      addon_version = "v1.22.6-eksbuild.1"
+    }
+  }
+
+  vpc_id     = module.vpc_bacchus_dev.vpc_id
+  subnet_ids = module.vpc_bacchus_dev.private_subnets
+
+  cluster_enabled_log_types = []
+
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t3a.medium"]
+    iam_role_additional_policies = [
+      "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    ]
+  }
+
+  eks_managed_node_groups = {
+    workers = {
+      name = "bacchus-dev-workers"
+
+      create_launch_template = false
+      launch_template_name   = ""
+
+      disk_size = 50
+
+      max_size     = 2
+      desired_size = 2
+    }
+  }
+
+  manage_aws_auth_configmap = true
+
+  # TODO: do this for each admin user
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::642254835236:user/tirr",
+      username = "tirr",
+      groups   = ["system:masters"]
+    }
+  ]
+}
+
+# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009#issuecomment-1096628912
+provider "kubernetes" {
+  host                   = module.eks_bacchus_dev.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_bacchus_dev.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks_bacchus_dev.cluster_id]
+  }
+}
