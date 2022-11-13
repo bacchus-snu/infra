4739464ca7355071c9c3f6be49c98b38211da804
Pyeongseok Oh <cseteram.dev@gmail.com>
2022-11-13 20:53:47 +0900

    Remove bacchus-dev vpc

--- vpc.tf
+++ vpc.tf
@@ -24,29 +24,6 @@ module "vpc_bartender" {
   }
 }
 
-module "vpc_bacchus_dev" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.13.0"
-
-  name = "bacchus-dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c", "ap-northeast-2d"]
-  private_subnets = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19"]
-  public_subnets  = ["10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19", "10.0.224.0/19"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1",
-  }
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1",
-  }
-}
-
 resource "aws_default_vpc" "bacchus" {
   tags = {
     Name = "default"
