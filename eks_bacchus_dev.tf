40ab61baf934bc79e3a7d6d82160a02b062b8967
Pyeongseok Oh <cseteram.dev@gmail.com>
2022-11-13 18:38:05 +0900

    Remove lbc policy

--- eks_bacchus_dev.tf
+++ eks_bacchus_dev.tf
@@ -45,25 +45,6 @@ module "eks_bacchus_dev" {
 
   cluster_enabled_log_types = []
 
-#   eks_managed_node_group_defaults = {
-#     ami_type       = "AL2_x86_64"
-#     instance_types = ["t3.medium"]
-#     iam_role_additional_policies = [
-#       "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-#     ]
-#   }
-
-#   eks_managed_node_groups = {
-#     workers = {
-#       name = "bacchus-dev-workers"
-# 
-#       disk_size = 50
-# 
-#       max_size     = 2
-#       desired_size = 2
-#     }
-#   }
-
   manage_aws_auth_configmap = true
 
   aws_auth_users = [for username in aws_iam_group_membership.bacchus_admin.users : {
@@ -71,32 +52,4 @@ module "eks_bacchus_dev" {
     username = username,
     groups   = ["system:masters"]
   }]
-}
-
-resource "aws_iam_role" "bacchus_dev_eks_lbc_assumerole" {
-  name = "BacchusDevEKSLoadBalancerControllerRole"
-
-  assume_role_policy = jsonencode({
-    "Version" = "2012-10-17",
-    "Statement" = [
-      {
-        "Effect" = "Allow",
-        "Principal" = {
-          "Federated" = module.eks_bacchus_dev.oidc_provider_arn,
-        },
-        "Action" = "sts:AssumeRoleWithWebIdentity",
-        "Condition" = {
-          "StringEquals" = {
-            "${module.eks_bacchus_dev.oidc_provider}:aud" = "sts.amazonaws.com",
-            "${module.eks_bacchus_dev.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller",
-          },
-        },
-      },
-    ],
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "bacchus_dev_eks_lbc" {
-  role       = aws_iam_role.bacchus_dev_eks_lbc_assumerole.name
-  policy_arn = aws_iam_policy.aws_eks_lbc_policy.arn
 }
