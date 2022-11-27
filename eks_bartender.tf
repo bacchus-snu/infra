d76b5b69aced4d8ab72084b5e9be4157a5f9e648
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2022-11-27 20:06:02 +0900

    Add external-secrets IRSA role (#41)

--- eks_bartender.tf
+++ eks_bartender.tf
@@ -544,3 +544,18 @@ resource "helm_release" "argocd" {
     file("helm/argo-cd.yaml")
   ]
 }
+
+module "external_secrets_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.4"
+
+  role_name                      = "external-secrets"
+  attach_external_secrets_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks_bartender.oidc_provider_arn
+      namespace_service_accounts = ["external-secrets:external-secrets"]
+    }
+  }
+}
