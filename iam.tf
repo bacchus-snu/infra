94ae873da0cbbe63c6c5cb983b56f36b97039e23
Dongwook Kim <jeffry0930@snu.ac.kr>
2023-02-11 23:27:01 +0900

    Added user vexatone (#49)
    * Added user vexatone
    
    * Update iam.tf
    
    vexatone 위치 조정
    
    * Added user vexatone
    
    ---------
    
    Co-authored-by: Jeffrey Kim <vexatone160@gmail.com>
    Co-authored-by: Pyeongseok Oh <cseteram.dev@gmail.com>

--- iam.tf
+++ iam.tf
@@ -40,6 +40,11 @@ locals {
       is_admin        = true,
       console_enabled = true,
     },
+    "vexatone" = {
+      pgp_key         = file("./keys/vexatone.gpg"),
+      is_admin        = true,
+      console_enabled = true,
+    },
     "songmin" = {
       pgp_key         = "keybase:smintree99",
       is_admin        = true,
