9976271d59984dbbaa1b181be79fcc9dbd8c6f80
nevivurn <nevivurn@nevi.dev>
2023-03-11 17:35:31 +0900

    Revert "fix: acm requires only one record"
    This reverts commit d782160b8ba28c96a3e1e94edff02ff7d62ef7e5.

--- certs_bacchus_io.tf
+++ certs_bacchus_io.tf
@@ -11,16 +11,27 @@ resource "aws_acm_certificate" "bacchus_io" {
 }
 
 resource "cloudflare_record" "bacchus_io_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.bacchus_io.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   zone_id = cloudflare_zone.bacchus.id
 
-  name  = tolist(aws_acm_certificate.bacchus_io.domain_validation_options)[0].name
-  type  = tolist(aws_acm_certificate.bacchus_io.domain_validation_options)[0].type
-  value = tolist(aws_acm_certificate.bacchus_io.domain_validation_options)[0].record
+  name  = each.value.name
+  type  = each.value.type
+  value = each.value.record
 
   proxied = false
 }
 
 resource "aws_acm_certificate_validation" "bacchus_io" {
-  certificate_arn         = aws_acm_certificate.bacchus_io.arn
-  validation_record_fqdns = [cloudflare_record.bacchus_io_validation.hostname]
+  certificate_arn = aws_acm_certificate.bacchus_io.arn
+  validation_record_fqdns = [
+    for dvo in aws_acm_certificate.bacchus_io.domain_validation_options :
+    cloudflare_record.bacchus_io_validation[dvo.domain_name].hostname
+  ]
 }
