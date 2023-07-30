a77936c79e81d28d194572862859d4440a598ddb
Yongun Seong <nevivurn@nevi.dev>
2023-07-30 16:39:02 +0900

    Revert "feat: add dex oidc provider & readonly role (#98)"
    This reverts commit f25a3262896abda6d3da8a2b1f05f696a78771c5.

--- main.tf
+++ main.tf
@@ -13,11 +13,6 @@ terraform {
       version = "~> 4.32"
     }
 
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 4.1"
