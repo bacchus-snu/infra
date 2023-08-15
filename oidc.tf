c629256427f716a665140336be1b1162e2ac8254
Yongun Seong <nevivurn@nevi.dev>
2023-08-15 13:08:53 +0900

    Revert "feat: add dex oidc provider & readonly role (#98)" (#101)
    This reverts commit f25a3262896abda6d3da8a2b1f05f696a78771c5.

--- oidc.tf
+++ oidc.tf
@@ -1,34 +1,3 @@
-data "tls_certificate" "dex_cert" {
-  url = "https://auth.bacchus.io/dex"
-}
-resource "aws_iam_openid_connect_provider" "dex_provider" {
-  client_id_list  = ["bacchus-aws"]
-  thumbprint_list = [data.tls_certificate.dex_cert.certificates[0].sha1_fingerprint]
-  url             = "https://auth.bacchus.io/dex"
-}
-
-data "aws_iam_policy_document" "dex_readonly_oidc" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.dex_provider.arn]
-    }
-    condition {
-      test     = "ForAnyValue:StringEquals"
-      variable = "auth.bacchus.io/dex:groups"
-      values   = ["regular-members@bacchus.snucse.org"]
-    }
-  }
-}
-resource "aws_iam_role" "dex_readonly" {
-  name        = "dex-readonly"
-  description = "Role to be assumed with OIDC token"
-
-  assume_role_policy  = data.aws_iam_policy_document.dex_readonly_oidc.json
-  managed_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
-}
-
 resource "aws_iam_openid_connect_provider" "github_oidc_provider" {
   url = "https://token.actions.githubusercontent.com"
 
