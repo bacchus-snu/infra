6a8444f61f05aaf27cd6800e475f028ba4870932
Yongun Seong <nevivurn@nevi.dev>
2024-07-11 22:17:13 +0900

    feat(tunnel): add catch-all

--- tunnel.tf
+++ tunnel.tf
@@ -4,23 +4,27 @@ resource "random_id" "webhook_secret" {
 
 resource "cloudflare_tunnel" "webhook" {
   account_id = "9d0fe600126436ae84ee3f9ed2f60a9c"
-  name = "argocd-webhook"
-  secret = random_id.webhook_secret.b64_std
+  name       = "argocd-webhook"
+  secret     = random_id.webhook_secret.b64_std
   config_src = "cloudflare"
 }
 
 resource "cloudflare_tunnel_config" "webhook" {
   account_id = cloudflare_tunnel.webhook.account_id
-  tunnel_id = cloudflare_tunnel.webhook.id
+  tunnel_id  = cloudflare_tunnel.webhook.id
 
   config {
     ingress_rule {
       hostname = "argocd-webhook.bacchus.io"
-      path = "/api/webhook"
-      service = "https://argocd.bacchus.io"
+      path     = "/api/webhook"
+      service  = "https://argocd.bacchus.io"
       origin_request {
         http_host_header = "argocd.bacchus.io"
       }
+    }
+    # catch-all
+    ingress_rule {
+      service = "http_status:404"
     }
   }
 }
