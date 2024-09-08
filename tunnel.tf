c928585eaf16e2a551309640433aec6f0b4ceb0e
Yongun Seong <nevivurn@nevi.dev>
2024-09-08 16:30:33 +0900

    chore(tunnel): update resource types due to deprecation

--- tunnel.tf
+++ tunnel.tf
@@ -2,16 +2,15 @@ resource "random_id" "webhook_secret" {
   byte_length = 32
 }
 
-resource "cloudflare_tunnel" "webhook" {
+resource "cloudflare_zero_trust_tunnel_cloudflared" "webhook" {
   account_id = "9d0fe600126436ae84ee3f9ed2f60a9c"
   name       = "argocd-webhook"
   secret     = random_id.webhook_secret.b64_std
-  config_src = "cloudflare"
 }
 
-resource "cloudflare_tunnel_config" "webhook" {
-  account_id = cloudflare_tunnel.webhook.account_id
-  tunnel_id  = cloudflare_tunnel.webhook.id
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "webhook" {
+  account_id = cloudflare_zero_trust_tunnel_cloudflared.webhook.account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.webhook.id
 
   config {
     ingress_rule {
